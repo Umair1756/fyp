54bd7c63b891667ec3f2060fba91cc84e84ab2a9
Farhan Ahmed <farhanf7n@gmail.com>
2020-07-19 21:47:09 +0500

    Created Board Page

--- application/views/userHomePage/userHomePage.php
+++ application/views/userHomePage/userHomePage.php
@@ -1,57 +1,65 @@
 
 <div class="penal_area">
-        <div class="col-lg-12 col-md-3 d-lg-block d-md-block d-none">
-            <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false"><i class="fab fa-flipboard pr-2"></i> Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false"><i class="fab fa-500px pr-2"></i> Boards</a>
-                </li>
-            </ul>
-            <!-- <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <div class="nav-link p-0 pl-3 pt-1 pb-1 anchor_adj" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
-                    <a><i class="fab fa-flipboard pr-2"></i> Home</a>
+    <div class="container">
+        <div class="col-lg-12">
+            <div class="recents">
+                <div class="h5">
+                    <i class='far fa-clock'></i> Recently Viewed
                 </div>
-                <a class="nav-link p-0 pl-3 pt-1 pb-1 mt-2 anchor_adj" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false"><i class="fab fa-500px pr-2"></i> Boards</a>
-            </div> -->
-        </div>
-        <!-- <div class="col-lg-9 col-md-9 col-sm-12 r_tab_content">
-            <div class="tab-content" id="v-pills-tabContent">
-                <div class="tab-pane fade show " id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                    <p class="m-2"><i class="fas fa-history pr-2"></i> Recently Viewed</p>
-                    <div class="container mb-5">
-                        <div class="row">
-                            <div class="col-lg-3 col-md-4 col-sm-4">TAB 1</div>
-                            <div class="col-lg-3 col-md-4 col-sm-4">TAB 2</div>
-                            <div class="col-lg-3 col-md-4 col-sm-4">TAB 3</div>
+                <div class="col-lg-12 bg-info">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                        <div class="col-lg-4">
+                            Board 1
                         </div>
                     </div>
-                    <p class="m-2">
-                        <i class="fas fa-project-diagram pr-2"></i> Personal Boards
-                    </p>
-                    <div class="container">
-                        <div class="row">
-                            <a class="nav-link text-center col-md-5 col-sm-6  p-4 m-1 rounded-0 p_anchor_adj" type="button" data-toggle="modal" data-target=".bd-example-modal-lg">
-                                <i class="fas fa-plus pr-2"></i> Create new board
-                            </a>
-                            <a class="nav-link text-center col-lg-3 col-md-5 col-sm-6 p-4 ml-4 m-1 rounded-0 p_anchor_adj">
-                                Board 1 here
-                            </a>
-                            <a class="nav-link text-center col-lg-3 col-md-5 col-sm-6 p-4 ml-4 m-1 rounded-0 p_anchor_adj">
-                                Board 2 here
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                    Templates
-                </div>
-                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-                    Home
                 </div>
             </div>
-        </div> -->
+
+            <div class="recents">
+                <div class="h5">
+                    <i class='fa fa-user text-2b4949'></i> Personal Boards
+                </div>
+                <div class="col-lg-12 bg-info">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="recents">
+                <div class="h5">
+                    <i class='far fa-clock'></i> Team name
+                </div>
+                <div class="col-lg-12 bg-info">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                        <div class="col-lg-4">
+                            Board 1
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>        
         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <form action="<?php echo base_url('index.php/userHome/saveTitle'); ?>" method="POST">
                 <div class="modal-dialog modal-lg mt-5" style="width: 30%;">
